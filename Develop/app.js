a82a025a75e563b66e4339ac1da16ee73525abb9
sehouse <sehouse@umich.edu>
2020-06-10 16:17:32 -0400

    Manager and Employee lib js files are written and tested. They work.

--- Develop/app.js
+++ Develop/app.js
@@ -10,71 +10,91 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeList = [];
+
+const questionsStart = [
+    {
+        type: `list`,
+        name: `employeePosition`,
+        message: `What position is the employee whose information you wish to enter?`,
+        choices: [`Manager`, `Engineer`, `Intern`]
+    }
+];
+
+const questionsContinue = [
+    {
+        type: `list`,
+        name: `confirm`,
+        message: `Would you like to input another employee's information?`
+    }
+]
+
 const questionsManager = [
     {
-        type: 'input',
-        name: 'managerName',
-        message: 'Please input the first and last name of the Manager:'
+        type: `input`,
+        name: `managerName`,
+        message: `Please input the first and last name of the Manager:`
     },
     {
-        type: 'input',
-        name: 'managerIdNumber',
-        message: 'Please input the employee ID number of the Manager:'
+        type: `input`,
+        name: `managerIdNumber`,
+        message: `Please input the employee ID number of the Manager:`
     },
     {
-        type: 'input',
-        name: 'managerEmailAddress',
-        message: 'Please input the company email address of the Manager:'
+        type: `input`,
+        name: `managerEmailAddress`,
+        message: `Please input the company email address of the Manager:`
     },
     {
-        type: 'input',
-        name: 'managerOfficeNumber',
-        message: 'Please input the office number of the Manager:'
+        type: `input`,
+        name: `managerOfficeNumber`,
+        message: `Please input the office number of the Manager:`
     }
 ];
 
 const questionsEngineer = [
     {
-        type: 'input',
-        name: 'engineerName',
-        message: 'Please input the first and last name of the Engineer:'
+        type: `input`,
+        name: `engineerName`,
+        message: `Please input the first and last name of the Engineer:`
     },
     {
-        type: 'input',
-        name: 'engineerIdNumber',
-        message: 'Please input the employee ID number of the Engineer:'
+        type: `input`,
+        name: `engineerIdNumber`,
+        message: `Please input the employee ID number of the Engineer:`
     },
     {
-        type: 'input',
-        name: 'engineerEmailAddress',
-        message: 'Please input the company email address of the Engineer:'
+        type: `input`,
+        name: `engineerEmailAddress`,
+        message: `Please input the company email address of the Engineer:`
     },
     {
-        type: 'input',
-        name: 'engineerGithubUsername',
-        message: 'Please input the GitHub username of the Engineer:'
+        type: `input`,
+        name: `engineerGithubUsername`,
+        message: `Please input the GitHub username of the Engineer:`
     }
 ];
 
 const questionsIntern = [
     {
-        type: 'input',
-        name: 'internName',
-        message: 'Please input the first and last name of the Intern:'
+        type: `input`,
+        name: `internName`,
+        message: `Please input the first and last name of the Intern:`
     },
     {
-        type: 'input',
-        name: 'internIdNumber',
-        message: 'Please input the employee ID number of the Intern:'
+        type: `input`,
+        name: `internIdNumber`,
+        message: `Please input the employee ID number of the Intern:`
     },
     {
-        type: 'input',
-        name: 'internEmailAddress',
-        message: 'Please input the company email address of the Intern:'
+        type: `input`,
+        name: `internEmailAddress`,
+        message: `Please input the company email address of the Intern:`
     },
     {
-        type: 'input',
-        name: 'internSchoolName',
-        message: 'Please input the name of the school the Intern attends:'
+        type: `input`,
+        name: `internSchoolName`,
+        message: `Please input the name of the school the Intern attends:`
     }
 ];
+
--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,22 @@
-// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.role = `Employee`
+        this.email = email;
+    }
+    getName() {
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+    getRole() {
+        return this.role;
+    }
+    getEmail() {
+        return this.email;
+    }
+}
+
+module.exports = Employee;
