96a6a6ea22079825ffce97782c40470a703b7a95
sehouse <sehouse@umich.edu>
2020-06-10 15:09:14 -0400

    Creation of inquirer questions for Manager.

--- Develop/app.js
+++ Develop/app.js
@@ -10,26 +10,25 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const questionsManager = [
+    {
+        type: 'input',
+        name: 'managerName',
+        message: 'Please input the first and last name of the Manager.'
+    },
+    {
+        type: 'input',
+        name: 'managerIdNumber',
+        message: 'Please input the employee ID number of the Manager.'
+    },
+    {
+        type: 'input',
+        name: 'managerEmailAddress',
+        message: 'Please input the company email address of the Manager.'
+    },
+    {
+        type: 'input',
+        name: 'managerOfficeNumber',
+        message: 'Please input the office number of the Manager.'
+    }
+]
